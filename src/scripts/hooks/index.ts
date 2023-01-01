af277cbb27c9e4fa3afb849fe5dff9658ae0cc34
Hervé Darritchon <herve.darritchon@gmail.com>
2023-01-01 20:17:04 +0100

    Ajoute DsN configuration dans le système.

--- src/scripts/hooks/index.ts
+++ src/scripts/hooks/index.ts
@@ -1,4 +1,5 @@
 import { Init } from "@scripts/init";
+import { DiceSoNiceReady } from "./dice-so-nice-ready";
 import { Load } from "./load";
 import { Ready } from "./ready";
 import { Setup } from "./setup";
@@ -7,6 +8,7 @@ export const HooksExitNihilo = {
     listen(): void {
         const listeners: { listen(): void }[] = [
             Load, // Run this first since it's not an actual hook listener
+            DiceSoNiceReady,
             Init,
             Ready,
             Setup
